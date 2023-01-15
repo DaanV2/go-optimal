bcf43ada541b32c3c9ee9f605416b195f24f9c8c
DaanV2 <daanverstraten@hotmail.com>
2023-01-15 20:51:09 +0100

    Updated settings

--- settings.go
+++ settings.go
@@ -10,20 +10,7 @@ import (
 func init() {
 	tempTarget, found := env.Int64.LookupIf("CPU_CACHE_OPTIMAL_SIZE")
 	if !found {
-		switch cpu.GetDefaultCacheTarget() {
-		case cpu.CacheL1:
-			// 60% of the L1 cache
-			tempTarget = percentage(cpu.GetCacheSize(cpu.CacheL1), 60)
-
-		case cpu.CacheL2:
-			// 90% of the L2, L3 cache
-			tempTarget = percentage(cpu.GetCacheSize(cpu.CacheL1), 90)
-
-		default:
-			fallthrough
-		case cpu.CacheL3:
-			tempTarget = percentage(cpu.GetCacheSize(cpu.CacheL1), 95)
-		}
+		tempTarget = OptimalBytesForCache(cpu.GetDefaultCacheTarget())
 	}
 
 	targetSize = int64(tempTarget)
@@ -32,6 +19,27 @@ func init() {
 	cpu.GetCPUInfo().Print()
 	fmt.Println("Target cpu cache: ", cpu.GetDefaultCacheTarget())
 	fmt.Println("Target size:      ", targetSize)
+}
+
+func OptimalBytesForCache(cache cpu.CacheKind) int64 {
+	switch cache {
+	case cpu.CacheL1:
+		// 60% of the L1 cache
+		size := cpu.GetCacheSize(cpu.CacheL1)
+		return percentage(size, 60)
+
+	case cpu.CacheL2:
+		// 90% of the L2
+		size := cpu.GetCacheSize(cpu.CacheL2)
+		return percentage(size, 90)
+
+	default:
+		fallthrough
+	case cpu.CacheL3:
+		// 95% of the L3
+		size := cpu.GetCacheSize(cpu.CacheL3)
+		return percentage(size, 95)
+	}
 }
 
 func percentage(value int64, percentage int64) int64 {
